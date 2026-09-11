c3cd48308393164547294941c7e7067863babee9
justin-ace <78005628+justin-ace@users.noreply.github.com>
2026-09-11 14:44:52 +0800

    Delete assets directory

--- assets/js/app.js
+++
@@ -1,9 +0,0 @@
-(function () {
-  document.addEventListener("DOMContentLoaded", () => {
-    if (document.body.dataset.page === "cart" && window.CartPageApp) {
-      new window.CartPageApp();
-    } else if (window.ShopApp) {
-      new window.ShopApp();
-    }
-  });
-})();
